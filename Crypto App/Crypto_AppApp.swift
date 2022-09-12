8a2533e889003d46cc12bfb3b1e8b40684c93341
Aditya Wahyu Pratama <adityawahyu131@gmail.com>
2022-09-12 14:16:20 +0700

    Initial Commit

---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by Adit on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
