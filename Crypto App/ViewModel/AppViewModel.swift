ef24b99a0d17efc35c3aac233be09807610ce8b6
Aditya Wahyu Pratama <adityawahyu131@gmail.com>
2022-10-24 15:59:58 +0700

    init

---
+++ Crypto App/ViewModel/AppViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  AppViewModel.swift
+//  Crypto App
+//
+//  Created by Adit on 17/10/22.
+//
+
+import SwiftUI
+
+class AppViewModel: ObservableObject {
+    @Published var coins: [CryptoModel]?
+    @Published var currentCoin: CryptoModel?
+    
+    init(){
+        Task{
+            do{
+                try await fetchCryptoData()
+            }catch{
+                // HANDLE ERROR
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    // MARK: Fetching Crypto Data
+    func fetchCryptoData()async throws{
+        // MARK: Using Latest Async/Await
+        guard let url = url else{return}
+        let session = URLSession.shared
+        
+        let response = try await session.data(from: url)
+        let jsonData = try JSONDecoder().decode([CryptoModel].self, from: response.0)
+        
+        // Alternative For DispatchQueue Main
+        await MainActor.run(body: {
+            self.coins = jsonData
+            if let firstCoin = jsonData.first{
+                self.currentCoin = firstCoin
+            }
+        })
+    }
+}
+
